1edd78d60e03dc62dfcd4e9781bef288867a1cef
liamoehlman <liam.oehlman@sidelab.com>
2013-06-24 15:29:15 +1000

    Removoed long description

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,6 @@ maintainer       "Eric G. Wolfe"
 maintainer_email "wolfe21@marshall.edu"
 license          "Apache 2.0"
 description      "Installs and configures nfs, and NFS exports"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 name "nfs"
 version          "0.3.1"
 recipe "nfs", "Installs and configures nfs client components"
